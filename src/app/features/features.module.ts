229521a14c3eabb1ec2fc09c24aebd961bf05943
Luan Bukowitz Beluzzo <luan.buk@gmail.com>
2021-04-14 22:56:39 -0300

    Redirecionamento de app para home

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ArchInternalsModule } from '../arch/internals/arch-internals.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
+    HomeComponent
   ],
   imports: [
     ArchInternalsModule
